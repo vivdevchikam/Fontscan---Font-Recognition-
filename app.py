3f6582440d482ee7c210d8e142ec18b68809cea0
vivdevchikam <vivekuraiti940@gmail.com>
2025-11-09 18:14:26 +0530

    ok

--- app.py
+++ app.py
@@ -33,4 +33,5 @@ def home():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=os.environ.get("FLASK_DEBUG") == "1")
